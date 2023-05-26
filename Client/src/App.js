590a79bce7f757c7180d47691dee7f4fb3dfd33b
Saurav Pant <pantsaurav005@gmail.com>
2023-05-26 15:13:13 +0530

    tailwind Css setup Done

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Blood Donation Project</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
---
+++ Client/src/components/navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div>
+       
+
+    </div>
+  )
+}
+
+export default navbar
