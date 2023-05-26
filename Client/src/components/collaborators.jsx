6d9aa41e934582a860b81a53e311e8931607ae89
Saurav Pant <pantsaurav005@gmail.com>
2023-05-26 22:30:06 +0530

    our mission Approx done

---
+++ Client/src/components/collaborators.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const collaborators = () => {
+  return (
+    <div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default collaborators
