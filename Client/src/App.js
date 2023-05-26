93391cfb3483cfa1cfa5dc722ccdd7f2f9548fcb
Saurav Pant <pantsaurav005@gmail.com>
2023-05-26 15:14:12 +0530

    tailwind

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Blood Donation App</h1>
     </div>
   );
 }
