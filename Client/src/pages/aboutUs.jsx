2575b06dfa63382f897bf4602cf32b2139fa8074
vivekrawat21 <vivekrwt2111@gmail.com>
2023-05-26 23:14:39 +0530

    linked pages associated with nav

---
+++ Client/src/pages/aboutUs.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/navbar";
+
+const aboutUs = () => {
+  return (
+    <div>
+      <Navbar /> 
+    </div>
+  );
+};
+
+export default aboutUs;
---
+++ Client/src/pages/findBlood.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/navbar";
+
+const findBlood = () => {
+  return (
+    <div>
+      <Navbar /> 
+    </div>
+  );
+};
+
+export default findBlood;
