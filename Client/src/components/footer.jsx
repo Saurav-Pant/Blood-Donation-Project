fc016f5e82a67825617624a61f62b8053dfecc41
Saurav Pant <pantsaurav005@gmail.com>
2023-05-27 11:34:15 +0530

    motion added to buttons

--- Client/src/components/footer.jsx
+++ Client/src/components/footer.jsx
@@ -3,7 +3,17 @@ import React from 'react'
 const footer = () => {
   return (
     <div>
+        <div>
+          
 
+        </div>
+        <div>
+
+        </div>
+
+        <div>
+
+        </div>
       
         
     </div>
