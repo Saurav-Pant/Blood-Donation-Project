1e9fef995f2802721cc9753e91356acc05430c77
Saurav Pant <pantsaurav005@gmail.com>
2023-05-29 08:54:26 +0530

    logIn Button Done

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,9 +1,8 @@
 import React, { useContext } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home";
 import Login from "./pages/login";
-import Register from "./pages/register";
 import AboutUs from "./pages/aboutUs";
 import FindBlood from "./pages/findblood";
 import Navbar from "./components/navbar";
@@ -12,35 +11,27 @@ import { ThemeContext } from "./context/ThemeContext";
 
 const App = () => {
   const { theme } = useContext(ThemeContext);
-  const currentPath = window.location.pathname; // Get the current route path
-
-  // Check if the current route is not "/login"
-  const shouldDisplayNavbarAndFooter = currentPath !== "/login";
+  const location = useLocation(); // Get the current location
+  console.log(location);
 
   return (
-    <Router>
-      <div
-        style={{
-          backgroundColor: theme.background,
-          color: theme.color,
-        }}
-      >
-        {/* Conditionally render Navbar */}
-        {shouldDisplayNavbarAndFooter && <Navbar />}
+    <div
+      style={{
+        backgroundColor: theme.background,
+        color: theme.color,
+      }}
+    >
+      {location.pathname !== "/login" && <Navbar />}
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          {/* <Route path="/signup" element={<Register />} /> */}
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/find-blood" element={<FindBlood />} />
-          {/* <Route path="/Register-Doner" element={<RegisterDoner />} /> */}
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/find-blood" element={<FindBlood />} />
+      </Routes>
 
-        {/* Conditionally render Footer */}
-        {shouldDisplayNavbarAndFooter && <Footer />}
-      </div>
-    </Router>
+      {location.pathname !== "/login" && <Footer />}
+    </div>
   );
 };
 
