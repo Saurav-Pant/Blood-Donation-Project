7c00305eb7bef7ada60a855a271b639de174f3f7
Saurav Pant <pantsaurav005@gmail.com>
2023-05-29 15:11:41 +0530

    Update home.jsx

--- Client/src/pages/home.jsx
+++ Client/src/pages/home.jsx
@@ -2,12 +2,8 @@ import React from "react";
 import Hero from "../components/hero";
 import OurMission from "../components/OurMission";
 import Collaborators from "../components/collaborators";
-<<<<<<< HEAD
-import GetBlood from "../components/getBlood"
 import Footer from "../components/footer";
-=======
 import GetBlood from "../components/getBlood";
->>>>>>> 3eb6df69f54b568b1121bb594fa770f7459807b6
 
 const home = () => {
   return (
