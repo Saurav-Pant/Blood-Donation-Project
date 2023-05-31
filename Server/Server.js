993ef3d0a46253da029a0ec07f516a9ba609a5f1
Saurav Pant <pantsaurav005@gmail.com>
2023-05-31 06:17:42 +0530

    mongoDB starter

--- Server/Server.js
+++ Server/Server.js
@@ -4,6 +4,13 @@ const app = express();
 
 //MongoDB
 
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/MyDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 app.listen(8080, () => {
   console.log("Server is running on port 8080");
 });
