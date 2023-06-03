dffad9fd8df681d98a82c1a30829e3f85a81fcfd
Saurav Pant <pantsaurav005@gmail.com>
2023-06-03 11:29:25 +0530

    Reverting to previous

--- Server/Server.js
+++ Server/Server.js
@@ -1,24 +1,11 @@
 const express = require("express");
+
 const app = express();
 
-// Twilio
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const TWILIO_PHONE_NUMBER = process.env.TWILIO_PHONE_NUMBER;
+//MongoDB
 
-const twilio = require("twilio")(accountSid, authToken);
-
-twilio.messages
-  .create({
-    from: "TWILIO_PHONE_NUMBER",
-    to: "+917467808514",
-    body: "This is a testing message",
-  })
-  .then((message) => console.log(message.sid))
-  .catch((err) => console.log(err));
-
-// MongoDB
 const mongoose = require("mongoose");
+
 mongoose.connect("mongodb://localhost:27017/MyDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
