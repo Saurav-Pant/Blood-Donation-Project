52c21ba4654d6cee53fbcf42fdd39d7d84464c3b
Saurav Pant <pantsaurav005@gmail.com>
2023-06-03 11:37:32 +0530

    mongoDB added and protected

--- Server/Server.js
+++ Server/Server.js
@@ -1,17 +1,22 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const MONGO_URI = process.env.MONGO_URI;
 
 // MongoDB
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/MyDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-mongoose.connection.on("connected", () => {
-  console.log("Connected to MongoDB");
-});
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(8080, () => {
   console.log("Server is running on port 8080");
