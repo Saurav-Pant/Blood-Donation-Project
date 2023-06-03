b41519091022fadce9d36642cb9557f3de864635
Saurav Pant <pantsaurav005@gmail.com>
2023-06-03 11:32:42 +0530

    mongoDb protection

--- Server/Server.js
+++ Server/Server.js
@@ -4,7 +4,7 @@ const app = express();
 // MongoDB
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/MyDB", {
+mongoose.connect("mongodb://127.0.0.1:27017/MyDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
