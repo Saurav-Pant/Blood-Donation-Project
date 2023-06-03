e3ce5f1e66a7d45f5c12e7cc0a2723a2b07fe2b8
Saurav Pant <pantsaurav005@gmail.com>
2023-06-03 11:23:14 +0530

    twillo addition

--- Server/Server.js
+++ Server/Server.js
@@ -2,6 +2,22 @@ const express = require("express");
 
 const app = express();
 
+//Twilio
+const accountSid = process.env.TWILLO_ACCOUNT_SID;
+const authToken = process.env.TWILLO_AUTH_TOKEN;
+const TWILLO_PHONE_NUMBER = process.env.TWILLO_PHONE_NUMBER;
+
+const twilio = require("twilio")(accountSid, authToken);
+
+twilio.messages
+  .create({
+    from: TWILLO_PHONE_NUMBER,
+    to: "+917467808514",
+    body: "This is testing message",
+  })
+  .then((message) => console.log(message.sid))
+  .catch((err) => console.log(err));
+
 //MongoDB
 
 const mongoose = require("mongoose");
