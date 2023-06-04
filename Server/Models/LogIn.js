22d97873a490e59a31bdd2ef4bfa2c2d33740cc8
Saurav Pant <pantsaurav005@gmail.com>
2023-06-04 07:56:06 +0530

    Logic Started

--- Server/Models/LogIn.js
+++ Server/Models/LogIn.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  phoneNumber: {
+  Email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  Password: {
     type: String,
     required: true,
   },
