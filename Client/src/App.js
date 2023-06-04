050cb97d9970dd3fa5e4be0e7dd74e5e25c1671c
Saurav Pant <pantsaurav005@gmail.com>
2023-06-04 07:17:18 +0530

    Login Deleted

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,7 +2,6 @@ import React, { useContext } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home";
-import Login from "./pages/login";
 import AboutUs from "./pages/aboutUs";
 import FindBlood from "./pages/findBlood";
 import Navbar from "./components/navbar";
@@ -25,11 +24,12 @@ const App = () => {
         color: theme.color,
       }}
     >
-      {location.pathname !== "/SignUp" && <Navbar />}
+      {location.pathname !== "/SignUp" && location.pathname !== "/login" && (
+        <Navbar />
+      )}
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login " element={<Login />} />
         <Route path="/SignUp" element={<SignUp />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/find-blood" element={<FindBlood />} />
@@ -37,7 +37,9 @@ const App = () => {
         <Route path="/register-donor" element={<RegisterDonor />} />
       </Routes>
 
-      {location.pathname !== "/SignUp" && <Footer />}
+      {location.pathname !== "/SignUp" && location.pathname !== "/login" && (
+        <Footer />
+      )}
     </div>
   );
 };
