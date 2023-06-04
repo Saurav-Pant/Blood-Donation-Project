f7f0b82c2c35c0f707ddcbcb690968b071c01306
Saurav Pant <pantsaurav005@gmail.com>
2023-06-04 07:25:48 +0530

    Login Route Approx Done

--- Client/src/pages/login.jsx
+++ Client/src/pages/login.jsx
@@ -1,9 +1,148 @@
-import React from 'react'
+import React from "react";
+import LogIn from "../asset/LogIn.png";
+import { motion } from "framer-motion";
+import { BsFacebook } from "react-icons/bs";
+import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
 
 const login = () => {
   return (
-    <div>login</div>
-  )
-}
+    <div className="flex justify-evenly items-center h-screen bg-gray-100">
+      <motion.div
+        className="hidden md:block"
+        initial={{ opacity: 0, position: "relative", left: "-100px" }}
+        animate={{ opacity: 1, position: "relative", left: "0px" }}
+        transition={{ duration: 2 }}
+      >
+        <img
+          src={LogIn}
+          alt="logo"
+          className="h-96 w-96 rounded-3xl shadow-md hover:bg-red-50 transition-colors duration-500 ease-in-out"
+        />
+      </motion.div>
+      <motion.div
+        className="w-screen max-w-md bg-white rounded-lg shadow-lg p-6 flex flex-col md:flex-row justify-center items-center hover:bg-red-50 transition-colors duration-500 ease-in-out"
+        initial={{
+          opacity: 0,
+          position: "relative",
+          right: "-200px",
+        }}
+        animate={{
+          opacity: 1,
+          position: "relative",
+          right: "0px",
+        }}
+        transition={{ duration: 1 }}
+      >
+        <div className="">
+          <motion.form
+            initial={{
+              opacity: 0,
+              y: -100,
+            }}
+            animate={{
+              opacity: 1,
+              y: 0,
+            }}
+            transition={{ duration: 1 }}
+          >
+            <div className="mb-6">
+              <label
+                htmlFor="email"
+                className="block text-gray-700 text-sm font-bold mb-2"
+              >
+                Email address
+              </label>
+              <input
+                type="email"
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-center"
+                id="email"
+                placeholder="Email address"
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                htmlFor="password"
+                className="block text-gray-700 text-sm font-bold mb-2"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-center"
+                id="password"
+                placeholder="Password"
+              />
+            </div>
+            <div className="mb-6">
+              <button
+                type="submit"
+                className="w-full bg-red-400 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline hover:bg-red-500"
+              >
+                Sign in
+              </button>
+            </div>
+          </motion.form>
 
-export default login
+          <div className="flex items-center justify-center">
+            <div className="border-b border-gray-300 w-full mr-3"></div>
+            <span className="text-gray-500">OR</span>
+            <div className="border-b border-gray-300 w-full ml-3"></div>
+          </div>
+          <motion.div
+            className="flex items-center justify-center mt-6"
+            initial={{
+              opacity: 0,
+              position: "relative",
+              bottom: "-100px",
+            }}
+            animate={{
+              opacity: 1,
+              position: "relative",
+              bottom: "0px",
+            }}
+            transition={{ duration: 1 }}
+          >
+            <Link
+              href="#"
+              className="px-4 py-2 rounded-full flex items-center mr-2 hover:transform hover:-translate-y-1 transition duration-300"
+            >
+              <BsFacebook className="text-blue-500" size="30" />
+            </Link>
+
+            <Link
+              href="#"
+              className="px-4 py-2 rounded-full flex items-center hover:transform hover:-translate-y-1 transition duration-300"
+            >
+              <FcGoogle className="text-gray-700" size="30" />
+            </Link>
+          </motion.div>
+          <motion.div
+            className="mt-6 text-center"
+            initial={{
+              opacity: 0,
+              position: "relative",
+              left: "-250px",
+            }}
+            animate={{
+              opacity: 1,
+              position: "relative",
+              left: "0px",
+            }}
+            transition={{ duration: 2 }}
+          >
+            <span className="text-gray-700">Create an account</span>
+            <Link
+              to="/SignUp"
+              className="text-blue-500  font-bold pl-3 hover:text-red-400 transition-colors duration-300 ease-in-out "
+            >
+              SignUp
+            </Link>
+          </motion.div>
+        </div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default login;
