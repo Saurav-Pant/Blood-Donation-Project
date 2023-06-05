38798369d7005c6ab07cc4278b9a1ee70fcf92ae
Saurav Pant <pantsaurav005@gmail.com>
2023-06-05 14:53:37 +0530

    PNF update

--- Client/src/components/PageNotFound.jsx
+++ Client/src/components/PageNotFound.jsx
@@ -1,22 +1,18 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import error from "../asset/404.jpg"
+import React from 'react';
+import { Link } from 'react-router-dom';
+import error from "../asset/404.jpg";
 
 const PageNotFound = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-gray-800 mb-4">Page Not Found</h1>
-      <p className="text-lg text-gray-600 mb-8">
-        Oops The page you are looking for does not exist
+    <div className="flex flex-col items-center justify-center h-screen ">
+      <h1 className="text-4xl font-bold mb-4 animated-heading">Page Not Found</h1>
+      <p className="text-lg mb-8">
+        Oops! The page you are looking for does not exist.
       </p>
-      <img
-        src={error}
-        alt="404 Error"
-        className="w-64 h-64 mb-8"
-      />
+      
       <Link
-        href="/"
-        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-200"
+        to="/"
+        className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition duration-200"
       >
         Go back to homepage
       </Link>
