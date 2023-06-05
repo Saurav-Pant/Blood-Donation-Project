9570bc6078add623696e36dd5ea83c412d448929
Saurav Pant <pantsaurav005@gmail.com>
2023-06-05 15:35:09 +0530

    SignUp Model Done

--- Server/Models/SignUp.js
+++ Server/Models/SignUp.js
@@ -1,5 +1,20 @@
-const mongoose= require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema=new mongoose.Schema({
-    Name:
-})
+const userSchema = new mongoose.Schema({
+  Name: {
+    type: "string",
+    required: true,
+  },
+  Email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  Password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
