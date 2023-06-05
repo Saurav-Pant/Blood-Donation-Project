445dd580f12ae7073956c8afa0005137727707c3
Saurav Pant <pantsaurav005@gmail.com>
2023-06-05 15:01:55 +0530

    PNF animated

--- Client/src/components/PageNotFound.jsx
+++ Client/src/components/PageNotFound.jsx
@@ -4,15 +4,17 @@ import error from "../asset/404.jpg";
 
 const PageNotFound = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen ">
-      <h1 className="text-4xl font-bold mb-4 animated-heading">Page Not Found</h1>
+    <div className="flex flex-col items-center justify-center h-[90vh]">
+           <h1 className="text-4xl text-red-500 font-bold mb-4 animate-bounce delay-1000" >
+        Page Not Found
+      </h1>
+
       <p className="text-lg mb-8">
         Oops! The page you are looking for does not exist.
       </p>
-      
       <Link
         to="/"
-        className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition duration-200"
+        className="px-4 py-2 rounded-lg bg-red-300 hover:bg-red-500 transition-colors duration-200 "
       >
         Go back to homepage
       </Link>
