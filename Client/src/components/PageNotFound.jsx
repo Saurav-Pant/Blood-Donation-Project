3f6f98217b02b51cb314fcc524b8bac055905504
Saurav Pant <pantsaurav005@gmail.com>
2023-06-05 15:10:15 +0530

    PNF Added

--- Client/src/components/PageNotFound.jsx
+++ Client/src/components/PageNotFound.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { motion } from 'framer-motion';
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const PageNotFound = () => {
   return (
-    <motion.div className="flex flex-col items-center justify-center h-[90vh]"
-    initial={{ opacity: 0, y: -100 }}
-    animate={{ opacity: 1, y: 0 }}
-    transition={{ duration: 1, delay: 0.2 }}
+    <motion.div
+      className="flex flex-col items-center justify-center h-[90vh]"
+      initial={{ opacity: 0, y: -100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 1, delay: 0.2 }}
     >
-           <h1 className="text-4xl text-red-500 font-bold mb-4 animate-bounce delay-1000" >
+      <h1 className="text-4xl text-red-500 font-bold mb-4 animate-bounce delay-1000">
         Page Not Found
       </h1>
 
@@ -18,7 +19,7 @@ const PageNotFound = () => {
       </p>
       <Link
         to="/"
-        className="px-4 py-2 rounded-lg bg-red-300 hover:bg-red-500 transition-colors duration-200 "
+        className="px-4 py-2 rounded-lg bg-red-300 hover:bg-red-500 transition-colors duration-200  "
       >
         Go back to homepage
       </Link>
