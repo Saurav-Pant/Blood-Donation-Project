8060bd0921d34ac22c88cdc9231af00863d38c5a
Saurav Pant <pantsaurav005@gmail.com>
2023-06-10 23:25:55 +0530

    selection error resolved

--- Server/index.js
+++ Server/index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 //MongoDB
 mongoose
-  .connect("mongodb://localhost:27017/Blood-Donation", {
+  .connect("mongodb://127.0.0.1/Blood-Donation", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
