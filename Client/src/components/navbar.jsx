d77efe60c262bd6410b3b7099c597358095435bf
Saurav Pant <pantsaurav005@gmail.com>
2023-06-11 10:41:42 +0530

    styling added

--- Client/src/components/navbar.jsx
+++ Client/src/components/navbar.jsx
@@ -3,32 +3,34 @@ import { Link } from "react-router-dom";
 import { BiDonateBlood } from "react-icons/bi";
 import { ThemeContext } from "../context/ThemeContext";
 import { motion } from "framer-motion";
-import { AiFillCaretDown } from "react-icons/ai";
-import { AiFillCaretUp } from "react-icons/ai";
+import { AiFillCaretDown, AiFillCaretUp } from "react-icons/ai";
 
 const Navbar = () => {
   const [showDropdown, setShowDropdown] = useState(false);
 
   const handleclick = () => {
-    document.querySelector(".hamburger").classList.toggle("active");
-    document.querySelector(".nav-menu").classList.toggle("active");
-  };
+    document.querySelector(".hamburger").classList.toggle('active');
+    document.querySelector(".nav-menu").classList.toggle('active');
+  }
 
   const { theme, toggleTheme } = useContext(ThemeContext);
-
-  let barcolor, navcolor;
-  if (theme.background === "#000000") {
+  
+  let barcolor,navcolor;
+  if(theme.background === "#000000"){
     barcolor = "#fff";
     navcolor = "#000";
-  } else {
+  }
+  else{
     barcolor = "#000";
     navcolor = "#fff";
   }
 
+  
   const handleDropdownToggle = () => {
     setShowDropdown(!showDropdown);
   };
 
+  
   return (
     <motion.nav
       className="sticky top-0 z-10 px-4 py-2 flex items-center justify-between "
@@ -48,40 +50,36 @@ const Navbar = () => {
         </Link>
       </div>
       {/* Hide links in small devices */}
-      <ul
-        className=" font-mono text-xl sm:flex "
-        onClick={handleclick}
-        style={{ backgroundColor: navcolor }}
-      >
-        <li className="ml-8 hover:text-red-400 transition-colors duration-300 ">
+      <ul className=" font-mono text-xl sm:flex nav-menu" onClick={handleclick} style={{backgroundColor:navcolor}}>
+        <li className="ml-8 hover:text-red-400 transition-colors duration-300 nav-item">
           <Link to="/">Home</Link>
         </li>
-        <li className="ml-8 hover:text-red-400 transition-colors duration-300 ">
+        <li className="ml-8 hover:text-red-400 transition-colors duration-300 nav-item">
           <Link to="/about">About Us</Link>
         </li>
-        <li className="ml-8 hover:text-red-400 transition-colors duration-300 ">
+        <li className="ml-8 hover:text-red-400 transition-colors duration-300 nav-item">
           <Link to="/find-blood">Find Blood</Link>
         </li>
         <li
-          className="ml-8 transition-colors duration-300  relative"
+          className="ml-8 nav-item relative"
           onClick={handleDropdownToggle}
         >
-          <div className="flex items-center cursor-pointer">
-            <span className="mr-2">Register Now</span>
-            {showDropdown ? <AiFillCaretUp /> : <AiFillCaretDown />}
+          <div className="register-link flex items-center cursor-pointer">
+            <span className="register-text mr-2">Register Now</span>
+            {showDropdown ? (
+              <AiFillCaretUp className="register-icon" />
+            ) : (
+              <AiFillCaretDown className="register-icon" />
+            )}
           </div>
+          {/* Dropdown */}
           {showDropdown && (
             <ul className="absolute left-0 w-40  rounded shadow mt-2">
-              <li
-                className="hover:text-red-400 
-            transition-colors duration-300  px-4 py-2"
-              >
+              <li className="hover:text-red-400 transition-colors duration-300  px-4 py-2">
                 <Link to="/register-donor">Register as Donor</Link>
               </li>
-              <li className="hover:text-red-400 transition-colors duration-300 px-4 py-2">
-                <Link to="/register-organization">
-                  Register as Organization
-                </Link>
+              <li className="hover:text-red-400 transition-colors duration-300  px-4 py-2">
+                <Link to="/register-organization">Register as Organization</Link>
               </li>
             </ul>
           )}
@@ -96,7 +94,7 @@ const Navbar = () => {
           }}
           whileHover={{ opacity: 0.7, transition: { duration: 0.5 } }}
         >
-          Sign Up
+         Sign Up
         </motion.button>
       </Link>
       <motion.button
@@ -108,9 +106,9 @@ const Navbar = () => {
         {theme.icon}
       </motion.button>
       <div className="hamburger" onClick={handleclick}>
-        <span className="bar" style={{ backgroundColor: barcolor }}></span>
-        <span className="bar" style={{ backgroundColor: barcolor }}></span>
-        <span className="bar" style={{ backgroundColor: barcolor }}></span>
+        <span className="bar" style={{backgroundColor:barcolor}}></span>
+        <span className="bar" style={{backgroundColor:barcolor}}></span>
+        <span className="bar" style={{backgroundColor:barcolor}}></span>
       </div>
     </motion.nav>
   );
