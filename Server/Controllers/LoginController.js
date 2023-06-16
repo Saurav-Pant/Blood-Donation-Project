73150aafa27b3331a524ca3cf37e1704fdf06abf
Saurav Pant <pantsaurav005@gmail.com>
2023-06-16 10:58:13 +0530

    Jwt for login Done

--- Server/Controllers/LoginController.js
+++ Server/Controllers/LoginController.js
@@ -1,5 +1,12 @@
 const User = require("../Models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.JWT_SECRET, {
+    expiresIn: "3d",
+  });
+};
 
 const LoginUser = async (req, res) => {
   const { email, password } = req.body;
@@ -21,7 +28,12 @@ const LoginUser = async (req, res) => {
       return res.status(401).json({ message: "Invalid credentials" });
     }
 
-    res.status(200).json({ message: "Login successful" });
+    // Creating a token for the client
+    const token = createToken(user._id);
+    res.status(200).json({
+      token,
+      email: user.email,
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Internal server error" });
