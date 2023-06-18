72fb5d3a5a452fcf652c484beb59e027633c9c5b
Saurav Pant <pantsaurav005@gmail.com>
2023-06-18 16:32:32 +0530

    getting Dashboard Data through API

--- Client/src/components/Dashboard.jsx
+++ Client/src/components/Dashboard.jsx
@@ -1,17 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Dashboard = () => {
   const [donorData, setDonorData] = useState(null);
-
   useEffect(() => {
-    fetch('http://localhost:8080/api/donors')
-      .then(response => response.json())
-      .then(data => {
-        setDonorData(data);
-        console.log(data);
+    axios
+      .get("http://localhost:8080/api/donors")
+      .then((response) => {
+        setDonorData(response.data);
+        console.log(response.data)
       })
-      .catch(error => {
-        console.error('Error:', error);
+      .catch((error) => {
+        console.error("Error:", error);
       });
   }, []);
 
@@ -21,23 +21,33 @@ const Dashboard = () => {
       <div className="grid grid-cols-2 gap-4 w-96 bg-white rounded-lg shadow-lg p-6 bg-gradient-to-r from-red-500 to-red-700">
         {/* Render donorData values */}
         <div>
-          <label className="block text-gray-700 text-sm font-bold mb-2">Name</label>
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Name
+          </label>
           <p className="text-gray-800">{donorData?.name}</p>
         </div>
         <div>
-          <label className="block text-gray-700 text-sm font-bold mb-2">Blood Group</label>
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Blood Group
+          </label>
           <p className="text-gray-800">{donorData?.bloodGroup}</p>
         </div>
         <div>
-          <label className="block text-gray-700 text-sm font-bold mb-2">Age</label>
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Age
+          </label>
           <p className="text-gray-800">{donorData?.age}</p>
         </div>
         <div>
-          <label className="block text-gray-700 text-sm font-bold mb-2">Phone Number</label>
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Phone Number
+          </label>
           <p className="text-gray-800">{donorData?.phoneNumber}</p>
         </div>
         <div>
-          <label className="block text-gray-700 text-sm font-bold mb-2">Address</label>
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Address
+          </label>
           <p className="text-gray-800">{donorData?.address}</p>
         </div>
       </div>
