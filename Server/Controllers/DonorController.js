219e30afe4fb0c8b256d1ac0f180ee9dfc38622a
Saurav Pant <pantsaurav005@gmail.com>
2023-06-18 15:30:25 +0530

    Dashboard APi setted

--- Server/Controllers/DonorController.js
+++ Server/Controllers/DonorController.js
@@ -1,13 +1,24 @@
 const Donor = require("../models/Donor");
 
-const DonorController = async (req, res) => {
-  try {
-    const donor = new Donor(req.body);
-    await donor.save();
-    res.status(201).json({ message: "Donor registered successfully" });
-  } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
-  }
+const DonorController = {
+  register: async (req, res) => {
+    try {
+      const donor = new Donor(req.body);
+      await donor.save();
+      res.status(201).json({ message: "Donor registered successfully" });
+    } catch (error) {
+      res.status(500).json({ error: "Internal server error" });
+    }
+  },
+
+  getAllDonors: async (req, res) => {
+    try {
+      const donors = await Donor.find();
+      res.status(200).json(donors);
+    } catch (error) {
+      res.status(500).json({ error: "Internal server error" });
+    }
+  },
 };
 
 module.exports = DonorController;
--- Server/Routes/donorRoutes.js
+++ Server/Routes/donorRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const DonorController = require("../Controllers/DonorController");
 
-router.post("/donors", DonorController);
-router.get("/donors", DonorController);
+router.post("/donors", DonorController.register);
+router.get("/donors", DonorController.getAllDonors);
 
 module.exports = router;
