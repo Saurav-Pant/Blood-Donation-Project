53dd04879acaafbd20fdb48416cfdb0c153e8376
Saurav Pant <pantsaurav005@gmail.com>
2023-07-03 15:18:17 +0530

    Postman all good

--- Client/src/components/donorForm.jsx
+++ Client/src/components/donorForm.jsx
@@ -106,225 +106,225 @@ const DonorForm = () => {
     }
   };
 
-  const compulsory = <span className='text-red-600'>*</span>;
+  const compulsory = <span className="text-red-600">*</span>;
 
   return (
     <motion.form
       onSubmit={handleSubmit}
-      className='mx-auto max-w-xl'
+      className="mx-auto max-w-xl"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ delay: 0.3, duration: 0.7 }}
     >
-      <div className='flex items-center bg-gradient-to-r from-red-900 via-red-900 to-red-800 h-[11vh] mt-4 rounded w-full mb-[2vh]'>
-        <h1 className='text-white text-2xl font-bold ml-4'>
+      <div className="flex items-center bg-gradient-to-r from-red-900 via-red-900 to-red-800 h-[11vh] mt-4 rounded w-full mb-[2vh]">
+        <h1 className="text-white text-2xl font-bold ml-4">
           Register as donor
         </h1>
       </div>
-      <div className='shadow border-1 p-8'>
-        <div className='mb-5'>
-          <label htmlFor='firstName' className='w-full mb-[2vw] mt-2'>
+      <div className="shadow border-1 p-8">
+        <div className="mb-5">
+          <label htmlFor="firstName" className="w-full mb-[2vw] mt-2">
             <h1>First Name {compulsory}</h1>
           </label>
           <input
-            type='text'
-            id='firstName'
-            name='firstName'
+            type="text"
+            id="firstName"
+            name="firstName"
             value={formData.firstName}
             onChange={handleInputChange}
-            className='mb-[2vw] pl-2 border-2 border-gray-300 hover:border-red-800 flex-grow h-10 w-full'
-            placeholder='First'
+            className="mb-[2vw] pl-2 border-2 border-gray-300 hover:border-red-800 flex-grow h-10 w-full"
+            placeholder="First"
             required
           />
-          <label htmlFor='lastName' className='w-full mb-[2vw] mt-2 mr-1'>
+          <label htmlFor="lastName" className="w-full mb-[2vw] mt-2 mr-1">
             Last Name {compulsory}
           </label>
           <input
-            type='text'
-            id='lastName'
-            name='lastName'
+            type="text"
+            id="lastName"
+            name="lastName"
             value={formData.lastName}
             onChange={handleInputChange}
-            className=' mb-[2vw] pl-2 border-2 border-gray-300 hover:border-red-800 flex-grow h-10 w-full'
-            placeholder='Last'
+            className=" mb-[2vw] pl-2 border-2 border-gray-300 hover:border-red-800 flex-grow h-10 w-full"
+            placeholder="Last"
             required
           />
         </div>
-        <div className='mb-5'>
-          <label htmlFor='phone' className='w-full mb-[2vw] mt-2 mr-1'>
+        <div className="mb-5">
+          <label htmlFor="phone" className="w-full mb-[2vw] mt-2 mr-1">
             Phone {compulsory}
           </label>
           <input
-            type='text'
-            id='phone'
-            name='phone'
+            type="text"
+            id="phone"
+            name="phone"
             value={formData.phone}
             onChange={handleInputChange}
-            className='pl-2 border-2 border-gray-300 hover:border-red-800 flex-grow h-10 w-full mb-[2vw]'
-            placeholder='Phone Number'
+            className="pl-2 border-2 border-gray-300 hover:border-red-800 flex-grow h-10 w-full mb-[2vw]"
+            placeholder="Phone Number"
             required
           />
 
-          <label htmlFor='email' className='w-full mb-[2vw] mt-2 mr-1'>
+          <label htmlFor="email" className="w-full mb-[2vw] mt-2 mr-1">
             Email {compulsory}
           </label>
           <input
-            type='email'
-            id='email'
-            name='email'
+            type="email"
+            id="email"
+            name="email"
             value={formData.email}
             onChange={handleInputChange}
-            className='pl-2 border-2 border-gray-300 hover:border-red-800 flex-grow h-10 w-full mb-[2vw]'
-            placeholder='Email'
+            className="pl-2 border-2 border-gray-300 hover:border-red-800 flex-grow h-10 w-full mb-[2vw]"
+            placeholder="Email"
             required
           />
         </div>
-        <div className='mb-5'>
-          <label htmlFor='bloodGroup' className='w-full mb-[2vw] mt-2 mr-1'>
+        <div className="mb-5">
+          <label htmlFor="bloodGroup" className="w-full mb-[2vw] mt-2 mr-1">
             Blood Group {compulsory}
           </label>
           <select
-            id='bloodGroup'
-            name='bloodGroup'
+            id="bloodGroup"
+            name="bloodGroup"
             value={formData.bloodGroup}
             onChange={handleInputChange}
-            className='pl-2 border-2  border-gray-300 hover:border-red-600  h-10 w-full mb-[2vw] '
+            className="pl-2 border-2  border-gray-300 hover:border-red-600  h-10 w-full mb-[2vw] "
             required
           >
-            <option value=''>-- Select --</option>
-            <option value='A+'>A+</option>
-            <option value='A-'>A-</option>
-            <option value='B+'>B+</option>
-            <option value='B-'>B-</option>
-            <option value='O+'>O+</option>
-            <option value='O-'>O-</option>
-            <option value='AB+'>AB+</option>
-            <option value='AB-'>AB-</option>
+            <option value="">-- Select --</option>
+            <option value="A+">A+</option>
+            <option value="A-">A-</option>
+            <option value="B+">B+</option>
+            <option value="B-">B-</option>
+            <option value="O+">O+</option>
+            <option value="O-">O-</option>
+            <option value="AB+">AB+</option>
+            <option value="AB-">AB-</option>
           </select>
-          <label htmlFor='age' className=' mb-[2vw] mt-2 mr-1 w-full '>
+          <label htmlFor="age" className=" mb-[2vw] mt-2 mr-1 w-full ">
             Age {compulsory}
           </label>
           <input
-            type='number'
-            id='age'
-            name='age'
+            type="number"
+            id="age"
+            name="age"
             value={formData.age}
             onChange={handleInputChange}
-            className='pl-2 border-2 border-gray-300 hover:border-red-600 w-full h-10'
-            placeholder='Age'
+            className="pl-2 border-2 border-gray-300 hover:border-red-600 w-full h-10"
+            placeholder="Age"
             required
           />
         </div>
-        <div className='mb-5 '>
-          <label htmlFor='address' className=' mt-2 mr-[2vw] w-full'>
+        <div className="mb-5 ">
+          <label htmlFor="address" className=" mt-2 mr-[2vw] w-full">
             Address {compulsory}
           </label>
           <input
-            type='text'
-            id='address'
-            name='address'
+            type="text"
+            id="address"
+            name="address"
             value={formData.address}
             onChange={handleInputChange}
-            className='pl-2 border-2 border-gray-300 hover:border-red-800 h-[12vh] w-full'
-            placeholder='Address'
+            className="pl-2 border-2 border-gray-300 hover:border-red-800 h-[12vh] w-full"
+            placeholder="Address"
             required
           />
-          <label htmlFor='state' className='w-24 mt-2 w-full'>
+          <label htmlFor="state" className="w-24 mt-2 w-full">
             State {compulsory}
           </label>
           <select
-            id='state'
-            name='state'
+            id="state"
+            name="state"
             value={formData.state}
             onChange={handleInputChange}
-            className='pl-2 border-2 border-gray-300 hover:border-red-800 flex-grow h-10 w-full mb-[2vw]'
+            className="pl-2 border-2 border-gray-300 hover:border-red-800 flex-grow h-10 w-full mb-[2vw]"
             required
           >
-            <option value=''>-- Select --</option>
-            <option value='Uk'>Uttarakhand</option>
+            <option value="">-- Select --</option>
+            <option value="Uk">Uttarakhand</option>
             {/* Add options for states */}
           </select>
-          <label htmlFor='city' className='w-full mb-[14vh]'>
+          <label htmlFor="city" className="w-full mb-[14vh]">
             City {compulsory}
           </label>
           <select
-            id='city'
-            name='city'
+            id="city"
+            name="city"
             value={formData.city}
             onChange={handleInputChange}
-            className='pl-2 border-2 border-gray-300 hover:border-red-800 flex-grow h-10 w-full'
+            className="pl-2 border-2 border-gray-300 hover:border-red-800 flex-grow h-10 w-full"
             required
           >
-            <option value=''>-- Select --</option>
-            <option value='Almora'>Almora</option>
+            <option value="">-- Select --</option>
+            <option value="Almora">Almora</option>
             {/* Add options for cities */}
           </select>
         </div>
-        <div className='flex  mb-[3vw]'>
-          <label htmlFor='gender' className='w-24 mr-4'>
+        <div className="flex  mb-[3vw]">
+          <label htmlFor="gender" className="w-24 mr-4">
             Gender {compulsory}
           </label>
-          <div className='flex items-center'>
+          <div className="flex items-center">
             <input
-              type='radio'
-              id='male'
-              name='gender'
-              value='male'
+              type="radio"
+              id="male"
+              name="gender"
+              value="male"
               onChange={handleInputChange}
-              className='mr-1'
+              className="mr-1"
               required
             />
-            <label htmlFor='male'>Male</label>
+            <label htmlFor="male">Male</label>
           </div>
-          <div className='flex items-center ml-4'>
+          <div className="flex items-center ml-4">
             <input
-              type='radio'
-              id='female'
-              name='gender'
-              value='female'
+              type="radio"
+              id="female"
+              name="gender"
+              value="female"
               onChange={handleInputChange}
-              className='mr-1'
+              className="mr-1"
               required
             />
-            <label htmlFor='female'>Female</label>
+            <label htmlFor="female">Female</label>
           </div>
-          <div className='flex items-center ml-4'>
+          <div className="flex items-center ml-4">
             <input
-              type='radio'
-              id='other'
-              name='gender'
-              value='other'
+              type="radio"
+              id="other"
+              name="gender"
+              value="other"
               onChange={handleInputChange}
-              className='mr-1'
+              className="mr-1"
               required
             />
-            <label htmlFor='other'>Others</label>
+            <label htmlFor="other">Others</label>
           </div>
         </div>
-        <div className='mb-[4vh]  font-bold '>
+        <div className="mb-[4vh]  font-bold ">
           <label>
             <input
-              type='checkbox'
+              type="checkbox"
               checked={isChecked}
               onChange={handleCheckboxChange}
-              className='mr-6 '
+              className="mr-6 "
             />
             All the details which are filled by me are right and ethical.
           </label>
         </div>
         {error && (
           <div
-            className='absolute right-14 bottom-3 text-red-300 hover:text-red-500 animate-bounce 
+            className="absolute right-14 bottom-3 text-red-300 hover:text-red-500 animate-bounce 
         transition-all duration-500 ease-in-out
-        '
+        "
           >
             {error}
           </div>
         )}
-        <div className='flex justify-end'>
+        <div className="flex justify-end">
           <button
-            type='submit'
-            className='w-full sm:w-1/2 mx-auto bg-red-900 hover:bg-red-800 text-white font-bold py-2 px-4 rounded'
+            type="submit"
+            className="w-full sm:w-1/2 mx-auto bg-red-900 hover:bg-red-800 text-white font-bold py-2 px-4 rounded"
           >
             Register
           </button>
