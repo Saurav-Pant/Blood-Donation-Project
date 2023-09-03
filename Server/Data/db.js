be314ef0a367072de92636a1f2716de5ffc32704
Saurav Pant <pantsaurav005@gmail.com>
2023-09-03 06:45:53 +0530

    error debug in env

--- Server/Data/db.js
+++ Server/Data/db.js
@@ -10,3 +10,8 @@ exports.connectToDB = () => {
     .then(() => console.log("MongoDB connected"))
     .catch((err) => console.log(err));
 };
+
+// JWT_SECRET="Saurav012"
+// LOCAL_MONGO_URI="mongodb://127.0.0.1/Blood-Donation"
+// PORT=8080
+// MONGO_URI="mongodb+srv://pantsaurav005:saurav@cluster0.n95tvws.mongodb.net/"
