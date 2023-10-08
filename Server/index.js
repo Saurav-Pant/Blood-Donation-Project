f8a7efed27824dd5573319d5d02d426a2c68b808
Saurav Pant <pantsaurav005@gmail.com>
2023-10-08 23:56:19 +0530

    changed to index

--- Server/index.js
+++ Server/index.js
@@ -1,28 +1,8 @@
-require("dotenv").config({
-  path: "config.env",
+const { app } = require("./Server.js");
+const { connectToDB } = require("./Data/db");
+
+connectToDB();
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
-
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-
-// Middleware
-app.use(express.json());
-// For Preventing CORS error
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
-app.use(cookieParser());
-
-// Routes
-app.use("/api/users", require("./Routes/SignUp.js"));
-app.use("/api/users", require("./Routes/Login.js"));
-app.use("/api", require("./Routes/donorRoutes.js"));
-
-module.exports = {
-  app,
-};
