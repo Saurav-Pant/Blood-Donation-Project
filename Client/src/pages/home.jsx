af8a36c5cc3c29ebfea287eeb7872a111214ef42
Saurav Pant <pantsaurav005@gmail.com>
2023-10-24 18:19:33 +0530

    For later commented

--- Client/src/pages/home.jsx
+++ Client/src/pages/home.jsx
@@ -3,15 +3,12 @@ import Hero from "../components/hero";
 import OurMission from "../components/OurMission";
 import Collaborators from "../components/collaborators";
 import GetBlood from "../components/getBlood";
-import MatchingBloodGroup from "../components/MatchingBloodGroup";
 
 const home = () => {
   return (
     <div className="overflow-hide">
-      {/* Commmented stuff */}
       <Hero />
       <OurMission />
-      <MatchingBloodGroup />
       <Collaborators />
       <GetBlood />
       
