5768186ec2051e2cdd3721dd8a033bdc14d89188
root-0101 <unofficially111@gmail.com>
2024-05-11 23:21:55 +0530

    updated sign_in

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -11,7 +11,7 @@ const Signin = () => {
   return (
     <>
       <div className="flex justify-evenly items-center h-screen bg-gray-100 dark:bg-black w-full">
-        <div className="absolute top-16 left-8">
+        <div className="absolute top-16 left-4">
           <Link
             href="/"
             className="px-4 py-2 rounded-full font-bold bg-gradient-to-br h-20 w-40 transition-colors duration-300 ease-in-out"
@@ -31,9 +31,9 @@ const Signin = () => {
           />
         </div>
         <div
-          className="w-full bg-white dark:bg-[#181717] rounded-lg shadow-lg p-6 flex flex-col md:flex-row justify-center items-center hover:bg-red-50 transition-colors duration-500 ease-in-out"
+          className="w-screen max-w-md bg-white dark:bg-[#181717] rounded-lg shadow-lg p-6 flex flex-col md:flex-row justify-center items-center hover:bg-red-50 transition-colors duration-500 ease-in-out"
         >
-          <SignIn/>
+          <SignIn />
         </div>
       </div>
     </>
