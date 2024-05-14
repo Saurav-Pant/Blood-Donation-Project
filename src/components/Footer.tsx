8f38c0e6ee4563d0f0f67a2299900d099f195324
Apoorv Gupta <apoorv.gupta65@gmail.com>
2024-05-14 12:38:46 +0530

    added toggle button to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,19 @@
 import React from "react";
 import { BsGithub } from "react-icons/bs";
 import { RiTwitterXFill } from 'react-icons/ri'
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { Button } from "./ui/button";
+import { useTheme } from "next-themes";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import Link from "next/link";
 
 const Footer = () => {
+  const { setTheme } = useTheme();
   return (
     <footer className="py-10 shadow-2xl bg-slate-700 rounded-2xl text-white mt-6 mb-1 mx-4">
       <div className="container mx-auto px-4">
@@ -23,7 +33,29 @@ const Footer = () => {
           <div
             className="flex justify-center md:justify-end md:w-1/2 mt-4"
           >
-            <div className="flex space-x-8">
+
+            <div className="flex items-center space-x-8">
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="outline" size="icon" className="transform hover:-translate-y-1 transition-transform duration-300">
+                    <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                    <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                    <span className="sr-only">Toggle theme</span>
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  <DropdownMenuItem onClick={() => setTheme("light")}>
+                    Light
+                  </DropdownMenuItem>
+                  <DropdownMenuItem onClick={() => setTheme("dark")}>
+                    Dark
+                  </DropdownMenuItem>
+                  <DropdownMenuItem onClick={() => setTheme("system")}>
+                    System
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+
               <Link
                 href="https://github.com/Saurav-Pant/Blood-Donation-Project"
                 target="_blank"
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -95,7 +95,6 @@ const Navbar = () => {
   const { userId } = useAuth();
   const [menuOpen, setMenuOpen] = useState(false);
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-  const { setTheme } = useTheme();
 
   useEffect(() => {
     function handleResize() {
@@ -159,26 +158,6 @@ const Navbar = () => {
         )}
 
         <div className="flex justify-center items-center gap-10">
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="outline" size="icon">
-                <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                <span className="sr-only">Toggle theme</span>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuItem onClick={() => setTheme("light")}>
-                Light
-              </DropdownMenuItem>
-              <DropdownMenuItem onClick={() => setTheme("dark")}>
-                Dark
-              </DropdownMenuItem>
-              <DropdownMenuItem onClick={() => setTheme("system")}>
-                System
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
           {!userId ? (
             <div className="flex justify-between items-center">
               <Link href="/sign-up">
