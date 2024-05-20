4aeefb6675c6d2b674dd7bf68dc0377c8f83260e
Devvart1 <devvartkumarkumar@gmail.com>
2024-05-20 21:50:23 +0530

    thankYou page added

--- src/app/ThankYou/page.tsx
+++ src/app/ThankYou/page.tsx
@@ -1,18 +1,32 @@
-"use client"
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
-const ThankYou: React.FC = () => {
-    return (
-        <div 
-            className="flex flex-col items-center justify-center h-screen overflow-hidden"
-            style={{ overflowY: 'hidden', height: '100vh' }}
-        >
-            <div className="text-center p-6 rounded shadow-lg">
-                <h1 className="text-3xl font-bold text-red-600 mb-6">Thank you for your Gift!</h1>
-                <p className="text-lg mb-4">Your donation can save lives and bring hope to those in need.</p>
-            </div>
-        </div>
-    )
-}
+const ThankYou = () => {
+  return (
+    <motion.div
+      className="flex flex-col items-center justify-center h-[90vh]"
+      initial={{ opacity: 0, y: -100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 1, delay: 0.2 }}
+    >
+      <h1 className="text-4xl text-red-500 font-bold mb-4 animate-bounce delay-1000">
+        Thank you for your Gift! 💕.
+      </h1>
+      <br />
+      <p className="text-lg mb-8">
+        Your donation can save lives and bring hope to those in need.
+      </p>
+      <Link
+        href="/"
+        className="px-4 py-2 rounded-lg bg-red-300 hover:bg-red-500 transition-colors duration-200  "
+      >
+        Go back to homepage
+      </Link>
+    </motion.div>
+  );
+};
 
 export default ThankYou;
-
