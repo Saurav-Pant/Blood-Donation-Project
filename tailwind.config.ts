27c7d2013727f0af511c9836af6c516e51edaf28
Aryan <aryansharma220318@gmail.com>
2024-06-03 19:02:04 +0530

    made the how to get blood cards responsive

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -40,6 +40,13 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+
+      fontSize: {
+        'xs': ['0.4rem', {
+          letterSpacing: '-0.01em',
+          fontWeight: '200',
+        }],
+      }
     },
   },
   plugins: [require("tailwindcss-animate"), addVariablesForColors],
